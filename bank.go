7569cb0314a89f38121e39859eae485e23833bc8
Roey Zalta <roey.zalta@gmail.com>
2024-10-12 11:04:09 +0300

    refactor - switch if statments with switch

--- bank.go
+++ bank.go
@@ -4,34 +4,47 @@ import "fmt"
 
 func main() {
 	var accountBalance float64 = 1000
-
 	fmt.Println("welcome to the bank")
-	fmt.Println("How can I help you?")
-	fmt.Println("1. check balance")
-	fmt.Println("2. deposit")
-	fmt.Println("3. withdraw")
-	fmt.Println("4. exit")
+	for {
+		fmt.Println("How can I help you?")
+		fmt.Println("1. check balance")
+		fmt.Println("2. deposit")
+		fmt.Println("3. withdraw")
+		fmt.Println("4. exit")
 
-	var choice int
-	fmt.Scan(&choice)
+		var choice int
+		fmt.Scan(&choice)
 
-	//wantsCheckBalance := choice == 1
+		//wantsCheckBalance := choice == 1
+		switch choice {
 
-	if choice == 1 {
-		fmt.Println("Your balance is: ", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Enter the amount you want to deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-		accountBalance += depositAmount
-		fmt.Println("Your updated balance is: ", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("Enter the amount you want to withdraw: ")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-		accountBalance -= withdrawAmount
-		fmt.Println("Your updated balance is: ", accountBalance)
-	} else {
-		fmt.Println("Thank you for visiting the bank")
+		case 1:
+			fmt.Println("Your balance is: ", accountBalance)
+			continue
+
+		case 2:
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount")
+				continue
+			}
+
+		case 3:
+			fmt.Print("Enter the amount you want to withdraw: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount")
+				continue
+			}
+
+		default:
+			fmt.Println("Goodbye")
+			fmt.Println("Thank you for visiting the bank")
+			return
+
+		}
 	}
 }
